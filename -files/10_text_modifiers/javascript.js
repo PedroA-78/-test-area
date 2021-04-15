787b49ef83690386b4adfd0e04bd56c68e135811
Pedro A. Ramos <pedro.rms031@gmail.com>
2021-04-15 00:09:41 -0300

    File 9 - Text modifiers (new part)

--- -files/10_text_modifiers/javascript.js
+++ -files/10_text_modifiers/javascript.js
@@ -1,17 +1,60 @@
 document.querySelector(".controls button").addEventListener("click", function(){
+    let event = 0
     const content = document.querySelector(".content")
-    let text = window.getSelection()
+    let selection = window.getSelection()
+    let text = content.innerHTML
 
-    const start = text.focusOffset > text.anchorOffset ? text.anchorOffset : text.focusOffset
-    const end = text.toString().length
-    // const end = text.anchorOffset > text.focusOffset ? text.anchorOffset : text.focusOffset
+    let start = selection.focusOffset > selection.anchorOffset ? selection.anchorOffset : selection.focusOffset
+    let end = selection.anchorOffset > selection.focusOffset ? selection.anchorOffset : selection.focusOffset
 
-    print("length: " + text.toString().length + " / start: " + start + " / end: " + end)
-    print(content.innerHTML.substr(start, end))
-    print(text.toString())
+    if (selection.toString().length == 0){
+        return
+    }
     
+    const bool = start > text.indexOf("</")
 
-    content.innerHTML = content.innerHTML.replace(content.innerHTML.substr(start, end), `<b>${text.toString()}</b>`)
+    print(start + " / " + end)
+
+    if (text.indexOf("</") != -1 && !bool){
+        event = text.toString().split("</").length - 1
+        print(event)
+
+        start += selection.toString().length + (7 * event)
+        end += selection.toString().length + (7 * event)
+    }
+
+    // if (text.indexOf("<b>") != -1 && !bool){
+    //     start += selection.toString().length + 7
+    //     end += selection.toString().length + 7
+    // }
+
+    print(start + " / " + end)
+
+    text = text.split('')
+
+    for (let i = start; i < end; i++){
+        if (selection.toString().length == 1){
+            text[i] = `<b>${text[i]}</b>`
+        } else if (i == start){
+            text[i] = `<b>${text[i]}`
+        } else if (i == end - 1){
+            text[i] = `${text[i]}</b>`
+        }
+    }
+
+    text = text.join("")
+    print(text)
+
+    content.innerHTML = text
+
+    // content.textContent = text
+
+    // print("length: " + text.toString().length + " / start: " + start + " / end: " + end)
+    // print(content.innerHTML.substr(start, end)) 
+
+    // content.textContent = content.innerHTML.replace(content.innerHTML.substr(start, end), `<b>${text.toString()}</b>`)
+
+    // text = text.empty()
     
 })
 
